0640e512c33fb70b2e7a1be7d6a327f782c775bc
Ксения Бурнашова <ksuhablondo@gmail.com>
2019-10-08 17:26:50 +0300

    HOC

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 import styles from './Counter.module.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+const Hoc = (Component) => {
+  return class extends React.Component {
+    render() {
+      return(
+        <Component {...this.props} variant="contained" className={styles.button}>
+          {this.props.children}
+        </Component>
+      );
+    }
+  }
+}
+
+const HocButton = Hoc(Button);
+
 
 const Counter = ({ count, onCountDown, onCountUp, resetAll }) => {
+  
   return(
     <div className={styles.count_wrap}>
       <span className={styles.count}>
         { count }
       </span>
-      <ButtonGroup variant="contained" size="small" aria-label="small contained button group">
-        <Button onClick={onCountDown}>-</Button>
-        <Button onClick={resetAll}>R</Button>
-        <Button onClick={onCountUp}>+</Button>
-      </ButtonGroup>
+      <div>
+        <HocButton onClick={onCountDown}>-</HocButton>
+        <HocButton onClick={resetAll}>R</HocButton>
+        <HocButton onClick={onCountUp}>+</HocButton>
+      </div>
     </div>
   );
 };
+
 
 export default Counter;
